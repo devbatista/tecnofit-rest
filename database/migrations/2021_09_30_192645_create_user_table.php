f88f15f45b3688cd780a28310ca9db03d01f2e24
Rafael Batista <rafael@devbatista.com>
2021-09-30 17:37:31 -0300

    criando as migrations

--- database/migrations/2021_09_30_192645_create_user_table.php
+++ database/migrations/2021_09_30_192645_create_user_table.php
@@ -3,8 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-class CreatePasswordResetsTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +14,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('user');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,4 @@ use App\Http\Controllers\RankingController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/ranking', [RankingController::class, 'listRanking']);
